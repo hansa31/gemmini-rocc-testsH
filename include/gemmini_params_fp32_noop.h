6f5b0b7a9865008c39f05419134782325ea2cc01
Seah Kim <kimsea8209@a16.Millennium.Berkeley.EDU>
2023-02-03 17:03:47 -0800

    saving linear algebra update

--- include/gemmini_params_fp32_noop.h
+++ include/gemmini_params_fp32_noop.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <limits.h>
 
-#define XCUSTOM_ACC 3
+#define XCUSTOM_ACC 0
 #define DIM 4
 #define ADDR_LEN 32
 #define BANK_NUM 4
@@ -14,26 +14,17 @@
 #define MAX_BLOCK_LEN (MAX_BYTES/(DIM*4))
 #define MAX_BLOCK_LEN_ACC (MAX_BYTES/(DIM*4))
 
-typedef float elem_t;
-static const elem_t elem_t_max = 3.4028235E38;
-static const elem_t elem_t_min = -3.4028235E38;
-typedef float acc_t;
-typedef double full_t;
-
-#define ELEM_T_IS_FLOAT
-#define ELEM_T_EXP_BITS 8
-#define ELEM_T_SIG_BITS 24
-#define ACC_T_EXP_BITS 8
-#define ACC_T_SIG_BITS 24
-typedef uint32_t elem_t_bits;
-typedef uint32_t acc_t_bits;
+typedef int32_t elem_t;
+static const elem_t elem_t_max = 2147483647;
+static const elem_t elem_t_min = -2147483648;
+typedef int32_t acc_t;
+typedef int64_t full_t;
 
 #define HAS_MVIN_SCALE
 typedef float scale_t;
 typedef uint32_t scale_t_bits;
 
-#define HAS_MVIN_ACC_SCALE
-typedef float scale_acc_t;
+typedef int32_t scale_acc_t;
 typedef uint32_t scale_acc_t_bits;
 
 typedef float acc_scale_t;
@@ -46,8 +37,11 @@ typedef uint32_t acc_scale_t_bits;
 
 #define ACC_SCALE_IDENTITY 1.0
 
+// Rounding right shift equation: https://riscv.github.io/documents/riscv-v-spec/#_vector_fixed_point_rounding_mode_register_vxrm
 #define ROUNDING_RIGHT_SHIFT(x, shift) \
-    ((x) / (1 << (shift)))
+    ((shift) > 0 ? (((x) >> (shift)) + \
+        (((shift) == 0 ? 0 : (((x) >> ((shift)-1)) & 1)) & \
+             ((((shift) <= 1 ? 0 : ((x) & ((1 << ((shift)-1)) - 1))) != 0) | (((x) >> (shift)) & 1)))) : ((x) << (-(shift))))
 
 #ifdef __cplusplus
 #define SAME_TYPE(x) decltype(x)
@@ -77,8 +71,7 @@ typedef uint32_t acc_scale_t_bits;
 #define MVIN_SCALE(x, scale) \
     ((x) * (scale))
 
-#define MVIN_SCALE_ACC(x, scale) \
-    ((x) * (scale))
+#define MVIN_SCALE_ACC(x, scale) (x)
 
 #define ACC_SCALE_T_IS_FLOAT
 #define ACC_SCALE_EXP_BITS 8
