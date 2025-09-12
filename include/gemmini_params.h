a085278fb2ba64928c66f6bdfdd7764358237561
hansa31 <hansaalahakoon@gmail.com>
2025-09-13 00:32:28 +1000

    coomit2

--- include/gemmini_params.h
+++ include/gemmini_params.h
@@ -8,8 +8,8 @@
 #define DIM 2
 #define ADDR_LEN 32
 #define BANK_NUM 4
-#define BANK_ROWS 8192
-#define ACC_ROWS 8192
+#define BANK_ROWS 4096
+#define ACC_ROWS 4096
 #define MAX_BYTES 64
 #define MAX_BLOCK_LEN (MAX_BYTES/(DIM*4))
 #define MAX_BLOCK_LEN_ACC (MAX_BYTES/(DIM*4))
